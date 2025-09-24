0f04b1f8ab1f016e0b8229dfbb0fdd3b303056a8
Abhishek Sharma <a2sharma97@gmail.com>
2025-09-24 09:44:22 +0530

    solved problem A of contest 424

---
+++ 424/424A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mx = INT_MAX;
+const int mn = INT_MIN;
+#define mod 1000000007
+#define yes printf("YES\n");
+#define no printf("NO\n");
+#define endl "\n"
+
+
+void solve(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a == b || b == c || a == c) {
+       cout << "Yes\n";
+    } else {
+       cout << "No\n";
+    }
+}
+
+
+signed main()
+{
+	ios::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);                     
+    int t=1;
+    // cin>>t;
+    for(int i=1;i<=t;i++){
+    	solve();
+    }
+}
